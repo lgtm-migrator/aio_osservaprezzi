aff988ff08067d1d6a76eb17a6d290845b9f8f63
Eliseo Martelli <me@eliseomartelli.it>
2020-08-08 12:30:24 +0200

    feat: parse into classes

--- aio_osservaprezzi/api.py
+++ aio_osservaprezzi/api.py
@@ -1,5 +1,5 @@
 from .const import ENDPOINT, REGIONS
-
+from .models import Station
 
 class API:
     def __init__(self, loop, session, data):
@@ -19,7 +19,7 @@ class API:
             headers={"Content-Type": "application/x-www-form-urlencoded"},
         )
         json = await r.json()
-        return json["array"]
+        return [Station.from_dict(s) for s in json["array"]] 
 
     async def get_data_by_id(self, id):
-        return next(filter(lambda d: d["id"] == id, await self.get_data()))
+        return next(filter(lambda d: d.id == id, await self.get_data()))
--- example.py
+++ example.py
@@ -14,6 +14,10 @@ async def test():
         data_by_id = await api.get_data_by_id(47715)
         print(data_by_id)
 
+        for fuel in data_by_id.fuels:
+            print(fuel)
+
+        #print (await api.get_data())
 
 loop.run_until_complete(test())
 loop.close()
